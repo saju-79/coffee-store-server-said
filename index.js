f0c265523b2757432945c8810cb73eb1703467fb
saju-79 <saju98622@gmail.com>
2026-01-19 07:04:14 +0600

    ditels-done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port =  process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
@@ -27,13 +27,26 @@ async function run() {
         const result =  await dataCollection.insertOne(coffee);
         res.send(result)
       });
-   app.get( "/users" , async(req ,res) =>{
+       app.get( "/users" , async(req ,res) =>{
             const result = await dataCollection.find().toArray();
             res.send(result)
-   })
+       });
 
+       app.delete("/users/:id" , async(req , res) =>{
+        const id = req.params.id;
+        const query = {_id : new ObjectId(id)};
+        const result = await dataCollection.deleteOne(query);
+        res.send(result)
+       });
 
+      app.get("/users/:id" , async(req , res) =>{
+        const id = req.params.id;
+        const query = {_id : new ObjectId(id)};
+        const result = await dataCollection.findOne(query);
+        res.send(result)
 
+      })
+     
 
 
 
